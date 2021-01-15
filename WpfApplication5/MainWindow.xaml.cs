c955703d4991cc503c816359819b55c08840e37f
Adrian Goessl <adrian.goessl@outlook.com>
2021-01-15 02:12:18 +0100

    Projektdateien hinzufügen.

---
+++ WpfApplication5/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void klick(object sender, RoutedEventArgs e)
+        {
+            button.Width = 100;
+            // Defines the camera used to view the 3D object. In order to view the 3D object,
+            // the camera must be positioned and pointed such that the object is within view 
+            // of the camera.
+            PerspectiveCamera myPCamera = new PerspectiveCamera();
+
+            // Specify where in the 3D scene the camera is.
+            myPCamera.Position = new Point3D(0, 0, 2);
+
+            // Specify the direction that the camera is pointing.
+            myPCamera.LookDirection = new Vector3D(0, 0, -1);
+
+            // Define camera's horizontal field of view in degrees.
+            myPCamera.FieldOfView = 60;
+
+            // Asign the camera to the viewport
+ //           myViewport3D.Camera = myPCamera;
+        }
+    }
+}
